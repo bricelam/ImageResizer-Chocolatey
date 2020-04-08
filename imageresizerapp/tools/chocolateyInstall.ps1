3ec38318d66c95ac082ca34f80473cf68e26ae5c
Brice Lambson <brice@bricelam.net>
2020-04-08 09:56:29 -0700

    Update to version 3.1.2

--- imageresizerapp/tools/chocolateyInstall.ps1
+++ imageresizerapp/tools/chocolateyInstall.ps1
@@ -2,6 +2,6 @@
     -PackageName imageresizerapp `
     -FileType exe `
     -SilentArgs '/quiet /norestart' `
-    -Url https://github.com/bricelam/ImageResizer/releases/download/v3.1.1/ImageResizerSetup-3.1.1.exe `
-    -Checksum 3C168AD78E2DE44F9B2BF12D26DE71096DE1995C6F5D8C7D2C67F29E3D60F1DE `
+    -Url https://github.com/bricelam/ImageResizer/releases/download/v3.1.2/ImageResizerSetup-3.1.2.exe `
+    -Checksum C239DB7D045AA0600A64FD4535E941E8750F21B3134F7DC7B2F6DE3385325798 `
     -ChecksumType sha256
